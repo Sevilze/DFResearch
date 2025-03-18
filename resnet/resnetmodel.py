6afb17cf607a825d7fd5ef74d487296e503d7963
Sevilze <sevilzcubing@gmail.com>
2025-03-18 23:48:48 +0700

    Refine resnet model and the trainer.

--- resnet/resnetmodel.py
+++ resnet/resnetmodel.py
@@ -1,24 +1,124 @@
 import torch.nn as nn
 import torchvision.models as models
+from torchvision.ops import StochasticDepth
 
-class ResNetClassifier(nn.Module):
-    def __init__(self, num_classes, pretrained=True):
-        super(ResNetClassifier, self).__init__()
-        self.resnet = models.resnet18(weights=models.ResNet18_Weights.IMAGENET1K_V1 if pretrained else None)
-        
-        for param in self.resnet.parameters():
-            param.requires_grad = True
-            
-        num_ftrs = self.resnet.fc.in_features
-        self.resnet.fc = nn.Sequential(
-            nn.Linear(num_ftrs, 512),
-            nn.ReLU(),
-            nn.Dropout(0.2),
-            nn.Linear(512, num_classes)
+
+class ChannelAttention(nn.Module):
+    def __init__(self, channels, reduction=16):
+        super().__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Sequential(
+            nn.Linear(channels, channels // reduction),
+            nn.ReLU(inplace=True),
+            nn.Linear(channels // reduction, channels),
+            nn.Sigmoid(),
         )
 
     def forward(self, x):
-        return self.resnet(x)
+        b, c, _, _ = x.size()
+        y = self.avg_pool(x).view(b, c)
+        y = self.fc(y).view(b, c, 1, 1)
+        return x * y.expand_as(x)
 
-def get_resnet_model(num_classes, pretrained=True):
-    return ResNetClassifier(num_classes, pretrained=pretrained)
+
+class ComplexBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1, dropout_prob=0.2):
+        super().__init__()
+        self.conv1 = nn.Conv2d(
+            in_channels,
+            out_channels,
+            kernel_size=3,
+            stride=stride,
+            padding=1,
+            bias=False,
+        )
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.conv2 = nn.Conv2d(
+            out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False
+        )
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.ca = ChannelAttention(out_channels)
+        self.dropout = nn.Dropout2d(p=dropout_prob)
+        self.stochastic_depth = StochasticDepth(p=0.2, mode="batch")
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_channels != out_channels:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(
+                    in_channels, out_channels, kernel_size=1, stride=stride, bias=False
+                ),
+                nn.BatchNorm2d(out_channels),
+            )
+
+    def forward(self, x):
+        identity = self.shortcut(x)
+        out = nn.ReLU()(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out = self.ca(out)
+        out = self.dropout(out)
+        out = self.stochastic_depth(out)
+        out += identity
+        return nn.ReLU()(out)
+
+
+class ResnetClassifier(nn.Module):
+    def __init__(self, num_classes, pretrained=True, use_complex_blocks=True):
+        super().__init__()
+        self.base = models.resnet50(
+            weights=models.ResNet50_Weights.IMAGENET1K_V2 if pretrained else None
+        )
+
+        if use_complex_blocks:
+            self._modify_resnet_architecture()
+
+        num_ftrs = self.base.fc.in_features
+        self.base.fc = nn.Sequential(
+            nn.Linear(num_ftrs, 1024),
+            nn.BatchNorm1d(1024),
+            nn.GELU(),
+            nn.Dropout(0.5),
+            nn.Linear(1024, 512),
+            nn.BatchNorm1d(512),
+            nn.GELU(),
+            nn.Dropout(0.3),
+            nn.Linear(512, num_classes),
+        )
+
+        self._init_weights()
+
+    def _modify_resnet_architecture(self):
+        self.base.layer2 = nn.Sequential(
+            ComplexBlock(512, 512, stride=2),
+            ComplexBlock(512, 512),
+            ComplexBlock(512, 512),
+            ComplexBlock(512, 512),
+        )
+
+        self.base.layer3 = nn.Sequential(
+            ComplexBlock(1024, 1024, stride=2),
+            ComplexBlock(1024, 1024),
+            ComplexBlock(1024, 1024),
+            ComplexBlock(1024, 1024),
+            ComplexBlock(1024, 1024),
+        )
+
+        self.base.layer4 = nn.Sequential(
+            ComplexBlock(2048, 2048, stride=2),
+            ComplexBlock(2048, 2048),
+            ComplexBlock(2048, 2048),
+        )
+
+    def _init_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode="fan_out", nonlinearity="relu")
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+    def forward(self, x):
+        return self.base(x)
+
+
+def ret_resnet(num_classes, pretrained=True):
+    return ResnetClassifier(num_classes, pretrained=pretrained)
--- resnet/trainer.py
+++ resnet/trainer.py
@@ -1,157 +1,172 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torchvision import transforms
+from torch.optim.lr_scheduler import OneCycleLR
+from torchvision import transforms, datasets
 import matplotlib.pyplot as plt
-import numpy as np
-from resnetmodel import get_resnet_model
-from torchvision import datasets
+from resnetmodel import ret_resnet
 
-class DataLoaderWrapper:
+class AdvancedDataLoader:
     def __init__(self, data_dir, batch_size=64, val_split=0.2):
         self.data_dir = data_dir
         self.batch_size = batch_size
         self.val_split = val_split
-        self.transform = transforms.Compose([
-            transforms.Resize((224, 224)),
+        
+        self.train_transform = transforms.Compose([
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomVerticalFlip(),
+            transforms.RandomRotation(30),
+            transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
+            transforms.RandomAffine(degrees=0, translate=(0.1, 0.1)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            transforms.RandomErasing(p=0.5, scale=(0.02, 0.33)), 
+        ])
+        
+        self.val_transform = transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         ])
         
-        full_dataset = datasets.ImageFolder(root=data_dir, transform=self.transform)
+        full_dataset = datasets.ImageFolder(root=data_dir)
         
         val_size = int(len(full_dataset) * val_split)
         train_size = len(full_dataset) - val_size
-        self.train_dataset, self.val_dataset = torch.utils.data.random_split(full_dataset, [train_size, val_size])
+        train_dataset, val_dataset = torch.utils.data.random_split(full_dataset, [train_size, val_size])
+        
+        train_dataset.dataset.transform = self.train_transform
+        val_dataset.dataset.transform = self.val_transform
         
         self.train_loader = torch.utils.data.DataLoader(
-            self.train_dataset, batch_size=batch_size, shuffle=True, num_workers=2)
+            train_dataset, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True)
         self.val_loader = torch.utils.data.DataLoader(
-            self.val_dataset, batch_size=batch_size, shuffle=False, num_workers=2)
+            val_dataset, batch_size=batch_size, shuffle=False, num_workers=4, pin_memory=True)
         
         self.class_names = full_dataset.classes
 
 class Trainer:
     def __init__(self, data_dir, num_classes, batch_size=64, lr=0.001):
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.data_wrapper = DataLoaderWrapper(data_dir, batch_size)
-        self.model = get_resnet_model(num_classes).to(self.device)
-        self.criterion = nn.CrossEntropyLoss()
-        self.optimizer = optim.Adam(self.model.parameters(), lr=lr)
-        self.class_names = self.data_wrapper.class_names
+        self.data_loader = AdvancedDataLoader(data_dir, batch_size)
+        self.model = ret_resnet(num_classes).to(self.device)
+        self._setup_training(lr)
+        self.class_names = self.data_loader.class_names
         
-    def imshow(self, imgs, labels, title):
-        imgs = imgs.cpu().numpy().transpose((0, 2, 3, 1))
-        mean = np.array([0.485, 0.456, 0.406])
-        std = np.array([0.229, 0.224, 0.225])
-        imgs = std * imgs + mean
-        imgs = np.clip(imgs, 0, 1)
+    def _setup_training(self, lr):
+        self.criterion = nn.CrossEntropyLoss(label_smoothing=0.1)
+        self.optimizer = optim.AdamW(self.model.parameters(), lr=lr, weight_decay=1e-4)
+        self.scheduler = OneCycleLR(
+            self.optimizer, 
+            max_lr=lr*10,
+            total_steps=None,
+            epochs=25,
+            steps_per_epoch=len(self.data_loader.train_loader),
+            pct_start=0.3,
+            anneal_strategy='cos'
+        )
+        self.scaler = torch.cuda.amp.GradScaler()
         
-        num_images = min(16, len(imgs))
-        grid_size = int(np.sqrt(num_images))
-        fig, axes = plt.subplots(grid_size, grid_size, figsize=(10, 10))
-        axes = axes.flatten()
-        
-        for i in range(num_images):
-            axes[i].imshow(imgs[i])
-            axes[i].set_title(self.class_names[labels[i]])
-            axes[i].axis('off')
-            
-        for j in range(num_images, len(axes)):
-            axes[j].axis('off')
-            
-        plt.suptitle(title)
-        plt.show()
-
-    def show_samples(self):
-        images, labels = next(iter(self.data_wrapper.train_loader))
-        self.imshow(images[:16], labels[:16], "Training Samples")
-        
-    def train_epoch(self):
+    def _train_epoch(self):
         self.model.train()
         running_loss = 0.0
         correct = 0
         total = 0
         
-        for inputs, labels in self.data_wrapper.train_loader:
+        for inputs, labels in self.data_loader.train_loader:
             inputs, labels = inputs.to(self.device), labels.to(self.device)
             
             self.optimizer.zero_grad()
-            outputs = self.model(inputs)
-            loss = self.criterion(outputs, labels)
-            loss.backward()
-            self.optimizer.step()
             
-            _, predicted = torch.max(outputs.data, 1)
+            with torch.cuda.amp.autocast():
+                outputs = self.model(inputs)
+                loss = self.criterion(outputs, labels)
+                
+            self.scaler.scale(loss).backward()
+            self.scaler.step(self.optimizer)
+            self.scaler.update()
+            self.scheduler.step()
+            
+            _, predicted = outputs.max(1)
             total += labels.size(0)
-            correct += (predicted == labels).sum().item()
+            correct += predicted.eq(labels).sum().item()
             running_loss += loss.item()
             
-        return running_loss/len(self.data_wrapper.train_loader), correct/total
+        return running_loss/len(self.data_loader.train_loader), correct/total
     
-    def validate(self):
+    def _validate(self):
         self.model.eval()
         val_loss = 0.0
         correct = 0
         total = 0
         
         with torch.no_grad():
-            for inputs, labels in self.data_wrapper.val_loader:
+            for inputs, labels in self.data_loader.val_loader:
                 inputs, labels = inputs.to(self.device), labels.to(self.device)
                 outputs = self.model(inputs)
                 loss = self.criterion(outputs, labels)
                 
-                _, predicted = torch.max(outputs.data, 1)
+                _, predicted = outputs.max(1)
                 total += labels.size(0)
-                correct += (predicted == labels).sum().item()
+                correct += predicted.eq(labels).sum().item()
                 val_loss += loss.item()
                 
-        return val_loss/len(self.data_wrapper.val_loader), correct/total
+        return val_loss/len(self.data_loader.val_loader), correct/total
     
-    def train(self, epochs=10):
+    def train(self, epochs=25):
         best_acc = 0.0
-        history = {'train_loss': [], 'train_acc': [], 'val_loss': [], 'val_acc': []}
+        history = {
+            'train_loss': [], 'train_acc': [],
+            'val_loss': [], 'val_acc': [],
+            'lr': []
+        }
         
         for epoch in range(epochs):
-            train_loss, train_acc = self.train_epoch()
-            val_loss, val_acc = self.validate()
+            train_loss, train_acc = self._train_epoch()
+            val_loss, val_acc = self._validate()
             
             history['train_loss'].append(train_loss)
             history['train_acc'].append(train_acc)
             history['val_loss'].append(val_loss)
             history['val_acc'].append(val_acc)
+            history['lr'].append(self.scheduler.get_last_lr()[0])
             
             print(f"Epoch {epoch+1}/{epochs}")
-            print(f"Train Loss: {train_loss:.4f} Acc: {train_acc:.4f}")
-            print(f"Val Loss: {val_loss:.4f} Acc: {val_acc:.4f}\n")
+            print(f"Train Loss: {train_loss:.4f} | Acc: {train_acc*100:.2f}%")
+            print(f"Val Loss: {val_loss:.4f} | Acc: {val_acc*100:.2f}%")
+            print(f"LR: {history['lr'][-1]:.6f}\n")
             
             if val_acc > best_acc:
                 best_acc = val_acc
-                torch.save(self.model.state_dict(), 'best_model.pth')
+                torch.save({
+                    'model_state': self.model.state_dict(),
+                    'optimizer_state': self.optimizer.state_dict(),
+                    'scheduler_state': self.scheduler.state_dict(),
+                }, 'best_model.pth')
                 
-        self.plot_training(history)
+        self._plot_training(history)
         
-    def plot_training(self, history):
-        plt.figure(figsize=(12, 5))
-        plt.subplot(1, 2, 1)
+    def _plot_training(self, history):
+        plt.figure(figsize=(18, 6))
+        
+        plt.subplot(1, 3, 1)
         plt.plot(history['train_loss'], label='Train Loss')
         plt.plot(history['val_loss'], label='Val Loss')
-        plt.legend()
         plt.title('Loss Curves')
+        plt.legend()
         
-        plt.subplot(1, 2, 2)
+        plt.subplot(1, 3, 2)
         plt.plot(history['train_acc'], label='Train Acc')
         plt.plot(history['val_acc'], label='Val Acc')
-        plt.legend()
         plt.title('Accuracy Curves')
+        plt.legend()
+        
+        plt.subplot(1, 3, 3)
+        plt.plot(history['lr'], label='Learning Rate')
+        plt.title('Learning Rate Schedule')
+        plt.legend()
+        
+        plt.tight_layout()
         plt.show()
-
-if __name__ == "__main__":
-    trainer = Trainer(
-        data_dir='path/to/your/dataset',
-        num_classes=10,
-        batch_size=64,
-        lr=0.001
-    )
-    trainer.show_samples()
-    trainer.train(epochs=15)
